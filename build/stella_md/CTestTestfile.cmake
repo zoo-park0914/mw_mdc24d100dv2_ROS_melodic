abd9a093b77e31b5a05dcb4ec569a5c7575a9bf1
zoo <kjooho0914@gmail.com>
2025-04-09 08:28:14 +0900

    fix dir to /catkin_ws/src/stella_md/

--- build/stella_md/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jnano/catkin_ws/src/stella_md
-# Build directory: /home/jnano/catkin_ws/build/stella_md
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/wt910_imu_reader/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jnano/catkin_ws/src/wt910_imu_reader
-# Build directory: /home/jnano/catkin_ws/build/wt910_imu_reader
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
